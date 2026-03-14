72f2abea24fa3bd1d71e325a9a1146572aeef0bf
roth-simon <rothsimon@seznam.cz>
2026-03-14 09:08:52 +0100

    Update layer_types.h

--- src/underground/layer_types.h
+++ src/underground/layer_types.h
@@ -1,1 +1,13 @@
+#pragma once
 
+enum UndergroundLayer
+{
+    LAYER_SURFACE = 0,
+    LAYER_UNDERGROUND_1 = 1,
+    LAYER_UNDERGROUND_2 = 2,
+    LAYER_UNDERGROUND_3 = 3,
+    LAYER_UNDERGROUND_ = 3
+
+};
+
+static const int MAX_UNDERGROUND_LAYERS = 5;
