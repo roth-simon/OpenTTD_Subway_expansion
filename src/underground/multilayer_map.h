16505735222ec77198ee77f7544c67fb9692da01
roth-simon <rothsimon@seznam.cz>
2026-03-30 16:24:37 +0200

    Refactor MultiLayerMap and add UndergroundTile structure
    Refactor MultiLayerMap structure to include UndergroundTile and update includes.

--- src/underground/multilayer_map.h
+++ src/underground/multilayer_map.h
@@ -1,20 +1,33 @@
 #pragma once
 
+#include "../stdafx.h"
 #include "../map_type.h"
-#include "layer_types.h"
+#include <cstdint>
 
-struct MultiLayerTile
+#define TOTAL_LAYERS 5
+
+enum UndergroundLayer
 {
-    Tile tiles[TOTAL_LAYERS];
+    LAYER_SURFACE = 0,
+    LAYER_U1,
+    LAYER_U2,
+    LAYER_U3,
+    LAYER_U4
+};
+
+struct UndergroundTile
+{
+    uint8_t type;
+    uint8_t transport;
+    uint8_t material;
 };
 
 class MultiLayerMap
 {
 public:
+    static void Initialize(uint32_t map_size);
+    static UndergroundTile &Get(TileIndex index, UndergroundLayer layer);
 
-    static MultiLayerTile *map;
-
-    static void Initialize(uint32 size);
-
-    static Tile &Get(TileIndex index, UndergroundLayer layer);
+private:
+    static UndergroundTile *_tiles;
 };
